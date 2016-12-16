bbc808ae764b2327eb8afe7158ab4cc3bfe8d974
Kail <kfkkail@gmail.com>
2016-12-16 15:55:30 +0000

    Finished tag feature

---
+++ app/controllers/tags_controller.rb
@@ -0,0 +1,33 @@
+class TagsController < ApplicationController
+    include TagsHelper
+    
+    def index
+        @tag = Tag.all
+    end
+
+    def show
+        @tag = Tag.find(params[:id])
+    end
+    
+    def edit
+        @tag = Tag.find(params[:id])
+    end
+    
+    def update
+        @tag = Tag.find(params[:id])
+        @tag.update(tag_params)
+        
+        flash.notice = "Article '#{@tag.name}' Updated!"
+        
+        redirect_to action: "index"
+    end
+    
+    def destroy
+        @tag = Tag.find(params[:id])
+        @tag.destroy
+        
+        flash.notice = "Article '#{@tag.name}' Deleted!"
+        
+        redirect_to action: "index"
+    end
+end
